8884277ecc5b6dfc89a0d764f326d21235703e10
Raul Cristian Ciurescu <24556350+ciurescuraul@users.noreply.github.com>
2025-07-09 12:41:53 +0300

    LinkedList nodes traversal

---
+++ hackerrank/src/main/java/hackerrank/algorithms/data_structures/ll/LinkedList.java
@@ -0,0 +1,71 @@
+package hackerrank.algorithms.data_structures.ll;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // constructor
+    public LinkedList(int value) {
+
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+    //
+    //    public Node RemoveLast() {
+    //    }
+    //
+    //    public void prepend(int value) {
+    //    }
+    //
+    //    public boolean insert(int index, int value) {
+    //    }
+    //
+    //    public Node remove(int index) {
+    //    }
+}
---
+++ hackerrank/src/main/java/hackerrank/algorithms/data_structures/ll/Main.java
@@ -0,0 +1,19 @@
+package hackerrank.algorithms.data_structures.ll;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.append(9);
+        myLinkedList.append(7);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+
+    }
+}
